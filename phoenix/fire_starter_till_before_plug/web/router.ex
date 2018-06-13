f80eeddc2390106114383fe6cf53d39c0a9fa835
Mudit Sharma <musharma@TSTK.local>
2018-06-13 15:14:49 +0530

    Add Phoenix more Functionlity

---
+++ phoenix/fire_starter_till_before_plug/web/router.ex
@@ -0,0 +1,30 @@
+defmodule FireStarter.Router do
+  use FireStarter.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", FireStarter do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    get "events", EventController, :list
+    get "events/new", EventController, :create
+    post "events/new", EventController, :add
+    get "events/:id", EventController, :show
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", FireStarter do
+  #   pipe_through :api
+  # end
+end
